d37851650e060d61d3b1bc5bbced86d7a36822a4
jakub <jakubkapusta39@gmail.com>
2018-11-07 18:56:46 +0100

    initial commit

---
+++ src/Liczby_losowe.java
@@ -0,0 +1,11 @@
+
+public class Liczby_losowe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		My_random random = new My_random(1, 11,5,3);
+		random.GeneratorLiniowy();
+	}
+
+}
---
+++ src/My_random.java
@@ -0,0 +1,30 @@
+
+public class My_random {
+	
+	private int a;
+	private int x0;
+	private int c;
+	private int m;
+	
+	public My_random(int a, int x0, int c, int m) {
+		this.a = a;
+		this.x0 = x0;
+		this.c = c;
+		this.m = m;
+	}
+	
+	public void GeneratorLiniowy() {
+		int xn=x0;
+		int i=0;
+		System.out.println("Generator liniowy");
+		
+		while(i!=m) {
+			i++;
+			xn=((a * xn) + c)%m;
+			System.out.println(xn);
+		}
+	}
+	
+	
+
+}
